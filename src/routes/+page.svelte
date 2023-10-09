c4ab15797046035e5f852af87e0b76b77a2441e3
Jose Von Chong <javch1995@gmail.com>
2023-10-09 13:52:47 -0500

    All files ready

---
+++ src/routes/+page.svelte
@@ -0,0 +1,67 @@
+<div class="w-full bg-primary min-h-[256px] grid grid-rows-[64px,auto,64px]">
+	<div class="row-start-2">
+		<div class="container mx-auto">	
+				<nav aria-label="Breadcrumb" class="!mb-24">
+					<ol class="Breadcrumb flex gap-2 md:gap-16 items-center">
+					<li class="text-grey-75 font-sans hover:underline active:underline">
+						<a href="copa.com">Home</a>
+					</li>
+					<li class="text-grey-75 font-sans">›</li>
+					<li class="text-grey-75 font-sans hover:underline active:underline">
+						<a href="destinationsguide.copaair.com/en/deals">Deals</a>
+					</li>
+			
+
+					</ol>
+				</nav>
+			<h5 class="font-heading font-heading-bold text-grey-75 text-h1-sm lg:text-h1-lg md:text-h1-md ">Copa Flash Sale</h5>	
+		</div>
+	</div>
+</div>
+
+<div class="w-full my-32">
+	<div class="container mx-auto">
+		
+	
+	<h2 class="!font-heading font-heading-bold text-primary text-h2-sm lg:text-h2-lg md:text-h2-md text-center ">¡Compra ya y ahorra! <br>
+	Nuestro Flash Sale te ofrece la oportunidad de volar a precios bajos. 
+	Aprovecha esta oferta y reserva tu próximo viaje.</h2>	
+
+	<h3 class="!font-heading font-heading-bold text-primary text-h3-sm lg:text-h3-lg md:text-h3-md text-center my-16">
+		Aprovecha esta oferta y reserva tu próximo viaje.</h3>
+
+
+		<h3 class="font-heading font-heading-bold text-primary-dark text-h3-sm lg:text-h3-lg md:text-h3-md text-center  my-32">
+			Guarda esta pagina en tu favoritos para futuras tarifas flash.</h3>
+	</div>
+</div>
+
+
+<div class="w-full my-32">
+	<div class="container mx-auto">
+		<h2 class=" !font-heading font-heading-bold text-primary text-h2-sm lg:text-h2-lg md:text-h2-md text-center">¡Celebra Cyber Monday con Copa!<br> ¡Diez destinos, con hasta 20% de descuento!</h2>
+		<h3 class=" !font-heading font-heading-bold !text-secondary text-h3-sm lg:text-h3-lg md:text-h3-md text-center my-16">Reserva desde el 
+			<br><span class=" text-primary">2 de octubre del 2023</span> hasta el  <span class=" text-primary">31 de octubre del 2023</span>
+		</h3>
+
+		<h3 class=" !font-heading font-heading-bold !text-secondary text-h3-sm lg:text-h3-lg md:text-h3-md text-center my-16">Para viajar desde 
+			<br><span class=" text-primary">1 de noviembre</span> hasta el  <span class=" text-primary">30 de noviembre del 2023</span>
+		</h3>
+
+
+			<div class="custom-pw-container mt-3 justify-content-center" data-add-functionalities=""
+			data-paginator-id="atlantaholderpaginator"
+			data-filters-id="[&quot;promocardholderoriginselect&quot;]">
+		
+		
+		
+		
+			</div>    
+
+			<p class="text-center">*Aplican restricciones, consulte nuestros <a
+				href="terminos-y-condiciones#kayakDays" target="_blank"
+				rel="noopener">T&eacute;rminos y
+				condiciones</a>.</p>
+
+	</div>
+</div>
